df121b37351fdc99f9089057da2e2aec84489dc2
unknown <jeromerichard72@yahoo.com>
2019-09-26 08:16:13 +0200

    ajout img,organisation lieux,maj test

---
+++ test.sql
@@ -0,0 +1,21 @@
+insert into groupe values (1001, 'Soul-X', "SOUL-X est un groupe de Thrash Metal né en 2002.
+Le quatuor participe à une multitude de concerts, festivals et tremplins aux côtés de nombreux groupes locaux, mais se frotte également à diverses pointures Metal tels que DAGOBA, NO FLAG, NASHVILLE PUSSY ou encore GOJIRA).
+Leur meilleur kiff reste à ce jour la première partie de LOUDBLAST à Calais, le 20 mai 2004. Un moment unique...
+Après un skeud 4 titres éponyme, des changements de line-up et un break, les voici de retour sur le devant de la scène Metal avec la ferme intention de réveiller les acouphènes des métalleux et autres buveurs de bières !
+Aujourd'hui constitué de Grég. Hespel (chant, guitare), Jérôme Leplat (guitare), Antoine Marlier (batterie / RADICAL SUCKER) et de Max. Lesur (basse), SOUL-X Represent avec un son lourd, puissant et sans compromis. Du pur Thrash Old School Style.
+La guerre ne fait que commencer...", 'soulxband@gmail.com', './sources/logo soulx.jpg' );
+
+insert into groupe values (1002, 'Bunker Project', "Si le groupe appartient à la grande famille du métal il se distingue par la variété des influences qui le nourrisse et en font toute la richesse (métal, funk, electro...).
+Forte de 5 membres aux profils aussi variés qu’indissociables la formation oscille entre métal et hardcore tout en explorant de nombreuses autres pistes, n'hésitant pas a se définir comme une entité proteiforme.
+Après avoir écumé pas mal de café-concert du Nord et de la Belgique le groupe finalise actuellement la sortie de son premier 6 titres.
+A découvrir !",'xabb59@gmail.com', './sources/logo bunker.jpg' );
+
+insert into groupe values (1003, 'Trepan Dead', "Le grind c'est un art. On ne dirait pas comme ça, mais balancer des  insanités à un rythme à peine tenable, avec une voix la plus désagréable  possible, avec en fond des psychopathes qui font
+ des misères à leurs  instruments, si si il y a de l'art là dedans. Et de l'art (et du  cochon...oui, elle était facile) il y en a dans la musique de Trepan'Dead, combo originaire de la paisible Valenciennes et aimant Inhumate, Rotten Sound,
+  Nasum, Napalm Death ou Brutal Truth.  Bon oui, forcément déjà les influences sont en acier massif. Du coup  avec une production XXL, un talent de composition bien frais et évident,  ça ne pouvait donner que du bon. (auxportedumétal)
+",'trepandead@orange.fr', './sources/logo trepan.jpg' );
+
+insert into groupe values (1004, 'BlackBart', "BlackBart est un groupe de Heavy Metal originaire de la région Lilloise. ",'', './sources/logo blackbart.png');
+
+insert into groupe values (1004, 'Modern Messiah', "We're a metalcore band from Belgium created in July 2016. Our sound combines the energy of modern metal with heavy metal's dense lead guitar work. All of it being supporter by a vast imagery",
+'mmbandcontact@gmail.com', './sources/logo-modern-messiah.jpg' );
